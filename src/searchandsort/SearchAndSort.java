566233adc1d29aac9a9b54f47aae397301530302
Elizabeth Gardner <>
2020-03-27 22:26:29 -0500

    I changed the selection sort and insertion sort methods to make them easier to understand, wrote a method to find the index of the first occurrence of a target number in a list of values through sequential search, and added a method that creates a list of random numbers in order to test the other methods.

--- src/searchandsort/SearchAndSort.java
+++ src/searchandsort/SearchAndSort.java
@@ -1,20 +1,63 @@
 package searchandsort;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
- * The class SearchAndSort contains methods to sort a list by
- * selection sort and insertion sort.
- * 
+ * The class SearchAndSort contains methods to sort a list by selection sort and
+ * insertion sort and a method to find the first occurrence of an integer in an
+ * ordered list using sequential search.
+ *
  * @author Elizabeth Gardner
- * @version 26 March 2020
+ * @version 27 March 2020
  */
 public class SearchAndSort {
 
-    // TO-DO: Define a method that determines
-    // the index of the first integer in a list
-    // of integers that matches a given integer.
-    // The method should return -1 if no match is found.
-    // Use the sequential search algorithm.
-    
+    // Random number generator borrowed from Professor Tabak.
+    private static Random rng = new Random();
+
+    /**
+     * The method makeList produces a list of random numbers that can be used to
+     * test the other methods of this class. Method borrowed from Professor
+     * Tabak.
+     *
+     * @param size
+     * @return list of random numbers
+     */
+    public static List<Integer> makeList(int size) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            int n = 10 + rng.nextInt(90);
+            result.add(n);
+        } // for
+
+        return result;
+    } // makeList( int )
+
+    /**
+     * The method firstIndexSequential finds the index of the first occurrence
+     * of a target number in a list of values through sequential search (going
+     * through the list and checking each value for equality) and returns -1 if
+     * the target number is not present in the list. Based on Professor Tabak's
+     * linearSearch but with a for loop instead of a while loop.
+     *
+     * @param values
+     * @param target
+     * @return int index of the first occurrence of the target number or -1
+     */
+    public static int firstIndexSequential(List<Integer> values,
+            int target) {
+        int result = -1;
+        for (int index = 0; index < values.size(); index++) {
+            if (values.get(index) == target) {
+                result = index;
+            } // if
+        } // for
+        return result;
+    } // firstIndexSequential
+
     // TO-DO: Define a method that determines
     // the index of the first integer in a list
     // of integers that matches a given integer.
@@ -22,24 +65,26 @@ public class SearchAndSort {
     // Use the binary search algorithm.
     
     /**
-     * The method selectionSort puts a list of integers in ascending order
-     * by looking for the smallest number in the unordered subset towards the end
-     * of the list and adding it to the ordered subset towards the beginning 
-     * of the list.
-     * 
-     * @param arr 
+     * The method selectionSort puts a list of integers in ascending order by
+     * looking for the smallest number in the unordered subset towards the end
+     * of the list and adding it to the ordered subset towards the beginning of
+     * the list.
+     * Adapted from Professor Tabak's example and code found here:
+     * https://www.javatpoint.com/selection-sort-in-java
+     *
+     * @param values : The list of numbers to be ordered.
      */
-    public static void selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
+    public static void selectionSort(List<Integer> values) {
+        for (int i = 0; i < values.size() - 1; i++) {
             int index = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[index]) {
-                    index = j;//searching for lowest index  
+            for (int j = i + 1; j < values.size(); j++) {
+                if (values.get(j) < values.get(index)) {
+                    index = j;
                 } // if
             } // for
-            int smallerNumber = arr[index];
-            arr[index] = arr[i];
-            arr[i] = smallerNumber;
+            int smallerNumber = values.get(index);
+            values.set(index, values.get(i));
+            values.set(i, smallerNumber);
         } // for
 
     } // selectionSort
@@ -48,54 +93,66 @@ public class SearchAndSort {
      * The method insertionSort puts a list of integers in ascending order by
      * checking if each number is larger than the one after it and, if so,
      * switching the numbers accordingly.
-     * 
-     * @param arr 
+     * Adapted from Professor Tabak's example and code found here:
+     * https://stackabuse.com/insertion-sort-in-java/
+     *
+     * @param arr
      */
-    public static void insertionSort(int[] arr) {
-        for (int j = 1; j < arr.length; j++) {
-            int key = arr[j];
+    public static void insertionSort(List<Integer> values) {
+        for (int j = 1; j < values.size(); j++) {
+            int current = values.get(j);
             int i = j - 1;
-            while ((i > -1) && arr[i] > key) {
-                arr[i + 1] = arr[i];
+            while ((i > -1) && ((values.get(i) > current))) {
+                values.set(i + 1, values.get(i));
                 i--;
             } // while
-            arr[i + 1] = key;
+            values.set(i + 1, current);
         } // for
     } // insertionSort
+
     // TO-DO: Define a method that sorts a list
     // of integers using the merge sort algorithm.
-
+    
     /**
-     * The main method first prints an unsorted list then prints the list after
-     * being sorted through selection sort and sorted through insertion sort.
-     * 
-     * @param args 
+     * The main method generates and prints a list of random numbers before and
+     * after being sorted by selection sort, another list before and after being
+     * sorted by insertion sort, and the first index of the number 18 in the
+     * previous sorted list.
+     *
+     * @param args
      */
     public static void main(String[] args) {
         System.out.println("Searching and sorting algorithms");
 
-        int[] arr1 = {9, 14, 3, 2, 43, 11, 58, 22};
-        System.out.println("Before Sorting");
-        for (int i : arr1) {
-            System.out.print(i + " ");
-        } // for 
-        System.out.println();
-
-        selectionSort(arr1);
-
-        System.out.println("After Selection Sort");
-        for (int i : arr1) {
-            System.out.print(i + " ");
-        } // for 
-        System.out.println();
-
-        int[] arr2 = {9, 14, 3, 2, 43, 11, 58, 22};
-
-        insertionSort(arr2);
-
-        System.out.println("After Insertion Sort");
-        for (int i : arr2) {
-            System.out.print(i + " ");
+        List<Integer> test = makeList(10);
+        System.out.println("Before selection sort:");
+        for (int element : test) {
+            System.out.print(element + " ");
         } // for
+        System.out.println();
+
+        selectionSort(test);
+        System.out.println("After selection sort:");
+        for (int element : test) {
+            System.out.print(element + " ");
+        } // for
+        System.out.println();
+
+        List<Integer> test2 = makeList(10);
+        System.out.println("Before insertion sort:");
+        for (int element : test2) {
+            System.out.print(element + " ");
+        } // for
+        System.out.println();
+
+        insertionSort(test2);
+        System.out.println("After insertion sort:");
+        for (int element : test2) {
+            System.out.print(element + " ");
+        } // for
+        System.out.println();
+
+        System.out.println("Index of the first occurance of 18 in the above list:");
+        System.out.println(firstIndexSequential(test2, 18));
     } // main( String [] )
 } // SearchAndSort
